9d72d5184a9e52653f996dc01213581974352a5b
sam cooper <98327570+sammy-kablammy@users.noreply.github.com>
2022-02-09 20:09:56 +0000

    numbers and colors and stuff; also fixed bug in generateEmptySpaces()

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ var canvas = document.getElementById("canv");
 var ctx = canvas.getContext("2d");
 var debugLabel = document.getElementById("debug");
 
-var numOfMines = 10;
+var numOfMines = 20;
 var gridColumnCount = 8;
 var gridRowCount = 8;
 var squareSize = 32;
+var textOffset = 7; // (in pixels) used to offset text (duh)
 
 var mouseX = 0;
 var mouseY = 0;
@@ -19,7 +20,10 @@ var grid = [];
   for(var r = 0; r < 10; r++) {
     grid[r] = [];
     for(var c = 0; c < 10; c++) {
-      grid[r][c] = 0;
+      grid[r][c] = {
+        val: 0,
+        hidden: false
+      }
     }
   }
 }
@@ -49,35 +53,33 @@ function generateMines() {
   while(minesGenerated < numOfMines) {
     var mineRow = Math.floor(Math.random() * gridColumnCount);
     var mineColumn = Math.floor(Math.random() * gridRowCount);
-    if(grid[mineRow][mineColumn] != "mine") {
-      grid[mineRow][mineColumn] = "mine";
+    if(grid[mineRow][mineColumn].val != "M") {
+      grid[mineRow][mineColumn].val = "M";
       minesGenerated++;
       console.log("mine generated at row: " + mineRow + " and column: " + mineColumn);
     }
   }
 }
 
+// figures out what number should be in each square based on # of mines near it
 function generateEmptySpaces() {
   for(var r = 0; r < 10; r++) {
     for(var c = 0; c < 10; c++) {
-      if(grid[r][c] != "mine") {
-        // figure out what number goes here, then put it here
+      if(grid[r][c].val != "M") {
+        // figure out what number goes in this square
         var mineCount = 0;
         for(var localR = -1; localR < 2; localR++) {
           for(var localC = -1; localC < 2; localC++) {
-            var isInBounds = (r + localR > 0 && r + localR < gridColumnCount && c + localC > 0 && c + localC < gridRowCount)
+            var isInBounds = (r + localR >= 0 && r + localR < gridColumnCount && c + localC >= 0 && c + localC < gridRowCount)
             if(isInBounds) {
-              if(grid[r + localR][c + localC] == "mine") {
+              if(grid[r + localR][c + localC].val == "M") {
                 mineCount++;
               }
             }
           }
         }
-
-        // now, put the mine count in the square
-        grid[r][c] = mineCount;
-
-
+        // now, put that number in the square
+        grid[r][c].val = mineCount;
       }
     }
   }
@@ -85,17 +87,66 @@ function generateEmptySpaces() {
 
 // this needs to be updated to account for non-mine squares
 function drawGrid() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   for(var r = 0; r < gridRowCount; r++) {
     for(var c = 0; c < gridColumnCount; c++) {
       ctx.beginPath();
+      // draw the covered squares
+      if(grid[r][c].hidden == true) {
+        ctx.rect(c * canvas.width / gridColumnCount, r * canvas.height / gridRowCount, squareSize, squareSize);
+        ctx.fillStyle = "grey";
+        ctx.fill();
+      }
+      // draw the uncovered squares
+      else { // hidden is false
+        ctx.font = "18px Arial";
+        ctx.textBaseline = "top";
+        // figure out what color the text should be
+        var color;
+        switch(grid[r][c].val) {
+          case 0:
+          color = "#eeeeee"; // to blend in with background color
+          break;
+          case 1:
+          color = "blue";
+          break;
+          case 2:
+          color = "green";
+          break;
+          case 3:
+          color = "red";
+          break;
+          case 4:
+          color = "darkBlue";
+          break;
+          case 5:
+          color = "darkRed";
+          break;
+          case 6:
+          color = "turquoise";
+          break;
+          case 7:
+          color = "black";
+          break;
+          case 8:
+          color = "darkGrey";
+          break;
+          default:
+          color = "purple"; // should never happen
+        }
+        ctx.fillStyle = color;
+        ctx.fillText(grid[r][c].val, (c * canvas.width / gridColumnCount) + textOffset, (r * canvas.height / gridRowCount) + textOffset);
+        ctx.fill();
+      }
+      // regardless of visibility, add black outline around the square
       ctx.rect(c * canvas.width / gridColumnCount, r * canvas.height / gridRowCount, squareSize, squareSize);
-
-      // this first if should be removed at some point
-      if(currentSquare.x == c && currentSquare.y == r) ctx.fillStyle = "blue";
-      else if(grid[r][c] == "mine") ctx.fillStyle = "red";
-      else ctx.fillStyle = "grey";
-
-      ctx.fill();
+      // if it's a mine, make red THEN draw text
+      if(grid[r][c].val == "M") {
+        ctx.fillStyle = "red";
+        ctx.fill();
+        ctx.fillStyle = "black";
+        ctx.fillText(grid[r][c].val, (c * canvas.width / gridColumnCount) + textOffset, (r * canvas.height / gridRowCount) + textOffset);
+      }
       ctx.strokeStyle = "black";
       ctx.stroke();
       ctx.closePath();
@@ -137,7 +188,7 @@ function resetGame() {
   // make all the squares blank
   for(var r = 0; r < 10; r++) {
     for(var c = 0; c < 10; c++) {
-      grid[r][c] = 0;
+      grid[r][c].val = 0;
     }
   }
 
@@ -148,6 +199,7 @@ function resetGame() {
   }
 
   console.log("clear stuff");
+  console.log("\n\n\n\n\n\n\n\n\n\n\n");
   console.clear(); // why doesnt this work ?????
   generateMines();
   generateEmptySpaces()
